22d6d9ff7560d47744ccb511a0c0ecf6d59e2623
I-am-Venkat <sathishboost3@gmail.com>
2025-02-21 12:41:21 +0530

    loginStarted&Home

---
+++ front-end/src/component/Home.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap/min.css';
+import {Link,useNavigate} from 'react-router-dom';
+import useState from 'react';
+const Home = () => {
+    return (
+        
+        <h1>Home</h1>
+        
+    )
+}
+export default Home;
--- front-end/src/component/Login.js
+++ front-end/src/component/Login.js
@@ -2,6 +2,19 @@ import "../styles/login.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link,useNavigate} from "react-router-dom";
 const Login=()=>{
+    const navigate=useNavigate();
+    const [formData,setFormData]=useSate({
+        mobilenumber:"",
+        password:""
+    })
+
+    const handleChange=()=>{
+        setFormData({...formData,[e.target.name]:e.target.value})
+    };
+
+    const handleSubmit=()=>{
+        
+    }
     return (
 
     <div class="container" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>
@@ -16,15 +29,15 @@ const Login=()=>{
         <form class="login_form" action="login.php" method="POST">
 
             <div class="form-group" id="mobile-number">
-                <input class="form-control" type="text" minlength="10" maxlength="10" placeholder="Enter Mobile number" required/>
+                <input className="form-control" type="text" name ="mobilenumber" value="formData.mobilenumber" minLength="10" maxLength="10" placeholder="Enter Mobile number" required/>
             </div>
 
             <div class="form-group" id="password">
-                <input class="form-control" type="password" minlength="8" placeholder="Enter Password" required/>
+                <input class="form-control" type="password" minLength="8" name="password" value="formData.password" placeholder="Enter Password" required/>
             </div>
 
             <div id="submit-button">
-                <button class="btn btn-primary" type="submit" id="submit">Submit</button>
+                <button class="btn btn-primary" type="submit" id="submit"><Link to='/Home'>Submit</Link></button>
             </div>
 
         </form>
