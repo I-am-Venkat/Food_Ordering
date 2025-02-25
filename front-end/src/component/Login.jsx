275b4256669dad5ef14bdcc00d46c4b23b8a072f
I-am-Venkat <sathishboost3@gmial.com>
2025-02-25 11:16:37 +0530

    jsx_format&Login_credentials

---
+++ front-end/src/component/Login.jsx
@@ -0,0 +1,63 @@
+import "../styles/login.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link,useNavigate} from "react-router-dom";
+import {useState} from "react";
+const Login=()=>{
+    const navigate=useNavigate();
+    const [formData,setFormData]=useState({
+        mobilenumber:"",
+        password:""
+    })
+
+   const handleChange=(e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value});
+   };
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        if((formData.password)!=="admin@123"){
+            alert("Wrong password");
+            return;
+        }
+        console.log("Login Successfull");
+        navigate("/Home"); 
+    }
+    return (
+
+    <div class="container" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>
+  
+
+    <div class="login-container">
+
+        <div id="heading">
+            <h2 style={{fontWeight:"800"}}>LOGIN</h2>
+        </div>
+        
+        <form class="login_form" action="login.php" onSubmit={handleSubmit} method="POST">
+
+            <div class="form-group" id="mobile-number">
+                <input className="form-control" type="text" name ="mobilenumber" onChange={handleChange} value={formData.mobilenumber} minLength="10" maxLength="10" placeholder="Enter Mobile number" required/>
+            </div>
+
+            <div class="form-group" id="password">
+                <input class="form-control" type="password" minLength="8" name="password" onChange={handleChange} value={formData.password} placeholder="Enter Password" required/>
+            </div>
+
+            <div id="submit-button">
+                <button class="btn btn-primary" type="submit" id="submit"><Link to='/Home'>Submit</Link></button>
+            </div>
+
+        </form>
+
+            <div id="redirect">
+                <label for="">Don't have an account <Link to="/">Create Account</Link></label>
+            </div>
+        
+        
+    </div>
+
+
+    </div>)
+
+}
+export default Login;
---
+++ front-end/src/component/Popup.jsx
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Popup=({show,onClose,title,msg})=>{
+    if(!show){
+        // setShowModel(false);
+        return null;
+    }
+    return (
+        <div className="modal show" style={{display:"block"}} tabIndex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              {title}
+            </h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={()=>setShowModal(false)}></button>
+          </div>
+
+          <div className="modal-body">
+            {msg}
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={()=>setShowModal(false)} >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Popup;
+// {showModal && (
+    
+//   )}
