3cd7c834ea2f39bdeb63bf4ed2a1bbad6da43414
I-am-Venkat <sathishboost3@gmial.com>
2025-03-20 20:03:11 +0530

    minuteChanges on ManageRestaurants

--- front-end/src/component/Dashboard/ManageRestaurants.jsx
+++ front-end/src/component/Dashboard/ManageRestaurants.jsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import { Popover } from '@mui/material';
-import Typography from '@mui/material';
+
 
 
 
@@ -20,12 +18,12 @@ const columns = [
   {
     field: 'ContactNumber',
     headerName: 'Contact Number',
-    type:'mobilenumber',
+    type: 'mobilenumber',
     width: 150,
   },
 ];
 
-const rows=[
+const rows = [
   { id: 1, RestaurantName: 'Maaran parotta kadai', City: 'Tirunelveli', OwnerName: 'Maaran', ContactNumber: 9874563210 },
   { id: 2, RestaurantName: 'A1 parotta stall', City: 'Tirunelveli', OwnerName: 'Riyaaz', ContactNumber: 9563147852 },
   { id: 3, RestaurantName: 'Vairamaaligai', City: 'Tirunelveli', OwnerName: 'Vairam', ContactNumber: 9445678901 },
@@ -40,152 +38,238 @@ const rows=[
 
 const paginationModel = { page: 0, pageSize: 5 };
 
-const DataTable= () => {
+const DataTable = () => {
   return (
-   
-   <>
-   <div className="addRestaurantDiv"    style={{
-          marginTop:"2%",
-          padding: '10px', 
-          display: 'flex', 
-          justifyContent: 'space-between', 
-          alignItems: 'center',
-          
-        }}>
-      <div style={{marginLeft:"5%"}}> <h2>Restaurants</h2> </div>
-      <div  style={{marginRight:"5%"}}> 
-      <button 
-							className="btn btn-dark" 
-							data-bs-toggle="modal"
-							data-bs-target="#myModal"
-						>
-							Add Restaurant 
-						</button>
-        {/* <Button variant="contained" id="addResBtn">Add Restaurant</Button> */}
+
+    <>
+      <div className="addRestaurantDiv" style={{
+        marginTop: "2%",
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+      }}>
+        <div style={{ marginLeft: "5%" }}> <h2>Restaurants</h2> </div>
+        <div style={{ marginRight: "5%" }}>
+          <button
+            className="btn btn-dark"
+            data-bs-toggle="modal"
+            data-bs-target="#myModal"
+            style={{ backgroundColor: "#0d6efd", color: "white", borderColor: "#0d6efd" }}
+          >
+            Add Restaurant
+          </button>
+          {/* <Button variant="contained" id="addResBtn">Add Restaurant</Button> */}
+        </div>
       </div>
-   </div>
- {/* Add Restaurant Modal */}
-   <div
-				className="modal fade"
-				id="myModal"
-				tabIndex="-1"
-				aria-labelledby="exampleModalLabel"
-				aria-hidden="true"
-				>
-				<div className="modal-dialog modal-lg modal-dialog-centered">
-					{/* Centered and Large Modal */}
-					<div className="modal-content">
-					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLabel">
-						Add Restaurant
-						</h5>
-						<button
-						type="button"
-						className="btn-close"
-						data-bs-dismiss="modal"
-						aria-label="Close"
-						></button>
-					</div>
+      {/* Add Restaurant Modal */}
+      <div
+        className="modal fade"
+        id="myModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg modal-dialog-centered">
+          {/* Centered and Large Modal */}
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add Restaurant
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
 
-					{/* Modal Body */}
-					<div className="modal-body">
-						<form>
-						<div className="row">
-							{/* First Row - Two Inputs */}
-							<div className="col-md-6 mb-3">
-							<label htmlFor="resName" className="form-label">
-								Restaurant Name
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								name="resName"
-								placeholder="Enter Restaurant Name"
-								id="resName"
-							/>
-							</div>
+            {/* Modal Body */}
+            <div className="modal-body">
+              <form>
+                <div className="row">
+                  {/* First Row - Two Inputs */}
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="resName" className="form-label">
+                      Restaurant Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="resName"
+                      placeholder="Enter Restaurant Name"
+                      id="resName"
+                      required
+                    />
+                  </div>
 
-							<div className="col-md-6 mb-3">
-							<label htmlFor="city" className="form-label">
-								City
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								name="city"
-								placeholder="Enter Restaurant City"
-								id="city"
-							/>
-							</div>
-						</div>
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="category" className="form-label">
+                      Categories
+                    </label>
+                    <br />
 
-						<div className="row">
-							{/* Second Row - Two More Inputs */}
-							<div className="col-md-6 mb-3">
-							<label htmlFor="ownerName" className="form-label">
-								Owner Name
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								name="ownerName"
-								placeholder="Enter Owner Name"
-								id="ownerName"
-							/>
-							</div>
+                    <div className="col-md-6 mb-3">
+                      <input class="form-check-input" type="checkbox" value="" id="veg" required />
+                      <label class="form-check-label" for="veg">
+                        &nbsp;&nbsp;Veg
+                      </label>
 
-							<div className="col-md-6 mb-3">
-							<label htmlFor="contact" className="form-label">
-								Contact Number 
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								name="contact"
-								placeholder="Enter Contact Number"
-								id="contact"
-							/>
-							</div>
-							<div className="btns"></div>
-							<div classname="col-md-6 mb-3">
-								<button class="btn btn-light">
-									Clear
-								</button>
-							</div>
-							<div classname="col-md-6 mb-3">
-								<button class="btn btn-dark">
-									Add
-								</button>
-							</div>
-						</div>
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-						
-						</form>
-					</div>
+                      <input class="form-check-input" type="checkbox" value="" id="nonVeg" />
+                      <label class="form-check-label" for="nonVeg">
+                        &nbsp;&nbsp;Non-Veg
+                      </label>
+                    </div>
+                  </div>
+
+
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="address" className="form-label">
+                      Address
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="address"
+                      placeholder="Enter Restaurant Address"
+                      id="address"
+                      required
+                    />
+                  </div>
+
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="city" className="form-label">
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="city"
+                      placeholder="Enter Restaurant City"
+                      id="city"
+                      required
+                    />
+                  </div>
+
+
+                  <div className="row">
+                    <div className="col-md-6 mb-3">
+                      <label htmlFor="pincode" className="form-label">
+                        Pincode
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="pincode"
+                        placeholder="Enter pincode"
+                        id="pincode"
+                        required
+                      />
+                    </div>
+
+
+                    {/* <div className="row"> */}
+                    {/* Second Row - Two More Inputs */}
+                    <div className="col-md-6 mb-3">
+                      <label htmlFor="ownerName" className="form-label">
+                        Owner Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="ownerName"
+                        placeholder="Enter Owner Name"
+                        id="ownerName"
+                        required
+                      />
+                    </div>
+
+                    <div className="col-md-6 mb-3">
+                      <label htmlFor="contact" className="form-label">
+                        Contact Number
+                      </label>
+                      <input
+                        type="phonenumber"
+                        className="form-control"
+                        name="contact"
+                        placeholder="Enter Contact Number"
+                        id="contact"
+                        minLength="10"
+                        maxLength="10"
+                        required
+                      />
+                    </div>
+
+
+                    <div className="col-md-6 mb-3">
+                      <label htmlFor="email" className="form-label">
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        placeholder="Enter Email ID"
+                        id="email"
+                      />
+                    </div>
+
+
+
+                    <div className="btnContainer" 
+                    style={{
+                      display:"flex" ,
+                      justifyContent:"space-between",
+                    }}
+                    >
+
+                      <div classname="col-md-6 mb-3">
+                        <button class="btn btn-light" style={{ backgroundColor: "#D84040" ,color:"white", marginLeft:"20px" ,width:"85px"}}>
+                          Clear
+                        </button>
+                      </div>
+                      <div classname="col-md-6 mb-3">
+                        <button class="btn btn-dark" style={{ backgroundColor: "#0d6efd" ,color:"white", marginRight  :"20px" ,width:"85px" ,borderColor:"#0d6efd"}}>
+                          Add
+                        </button>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+              </form>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
 
-    <Box sx={{
-      width:"90vw",
-      marginTop:"4%",
-      margin:"50px auto ",
-      display: "flex",
-      justifyContent: "center", // Centers horizontally
-      alignItems: "center"
-    }}>
-    <Paper sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-      
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        sx={{ border: 0 }}
-      />
-    </Paper>
-    </Box>
+      <Box sx={{
+        width: "90vw",
+        marginTop: "4%",
+        margin: "50px auto ",
+        display: "flex",
+        justifyContent: "center", // Centers horizontally
+        alignItems: "center"
+      }}>
+        <Paper sx={{ height: 400, width: '100%' }}>
+          <DataGrid
+
+            rows={rows}
+            columns={columns}
+            initialState={{ pagination: { paginationModel } }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+            sx={{ border: 0 }}
+          />
+        </Paper>
+      </Box>
     </>
   );
 }
