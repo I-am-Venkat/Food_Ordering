f9da3366dd802d7ae764fe908cfe93c506007b5a
I-am-Venkat <sathishboost3@gmail.com>
2025-04-01 12:43:14 +0530

    DBinitialized

--- back-end/server.js
+++ back-end/server.js
@@ -1,41 +1,54 @@
-// Server//
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
-const express=require("express");
-const cors=require("cors");
-const bodyParser=require("body-parser");
-require("dotenv") .config();
+import connectDB from "./config/db.js";
+
+// Server//
 
 const app=express();
 const PORT=process.env.PORT || 5000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors(
+    {
+        origin:"http://localhost:3000",
+        methods:["GET","POST","DELETE","PUT"],
+        credentials:true,
+        allowedHeaders:["Content-Type","Authorization"],
+    }
+));
+app.use(express.json());
+
+
 
 //Dummy DB
 
-const users=[
-    {mobilenumber:"9090909090",password:"sathish@123"},
-    {mobilenumber:"8825780899",password:"admin@123"}
-];
+// const users=[
+//     {mobilenumber:"9090909090",password:"sathish@123"},
+//     {mobilenumber:"8825780899",password:"admin@123"}
+// ];
 
-app.get("/",(req,res)=>{
-    res.send("Hello World !");
-})
+// app.get("/",(req,res)=>{
+//     res.send("Hello World !");
+// })
 
-app.post("/login",(req,res)=>{
-    const {mobilenumber,password}=req.body;
+// app.post("/login",(req,res)=>{
+//     const {mobilenumber,password}=req.body;
 
-    const user=users.find(u=>u.mobilenumber==mobilenumber && u.password==password);
+//     const user=users.find(u=>u.mobilenumber==mobilenumber && u.password==password);
         
-    if(user){
-        res.json({success:true,msg:"Login Successfull"});
-    }
-    else{
-        res.json({success:false,msg:"Invalid"});
-    }
-    })
+//     if(user){
+//         res.json({success:true,msg:"Login Successfull"});
+//     }
+//     else{
+//         res.json({success:false,msg:"Invalid"});
+//     }
+//     })
 
     app.listen(PORT,()=>{
+        connectDB();
         console.log(`Server running on https://localhost:${PORT}`);
     })
 
--- back-end/src/model/user.model.js
+++ back-end/src/model/user.model.js
@@ -1,3 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema 
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    mobilenumber:{
+        type:Number,
+        required:true,
+        unique:true,
+        minlength:10,
+        maxlength:10,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8,
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minlength:8,
+    },
+},
+{
+    timestamps:true,
+});
+
+const User=mongoose.model("User",userSchema);
+export default User;
+
