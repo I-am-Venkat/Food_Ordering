72f2f17b86adff10823915cb7253a66e47c5e48c
I-am-Venkat <sathishboost3@gmial.com>
2025-04-18 05:49:56 +0530

    react imported for termux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,6 @@
+import react from 'react';
 import logo from './logo.svg';
+
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
